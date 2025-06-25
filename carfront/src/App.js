decfe516a986f5df3527f2344c3770f2f5d93577
dew <foaming21@gmail.com>
2025-06-25 17:01:58 +0900

    변경사항 정리

--- carfront/src/App.js
+++ carfront/src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 // import MainApp from './ex01_MainApp/MainApp';
 // import MainApp from './ex02_CarList/MainApp';
-import MainApp from './ex03_ServerUrl/MainApp';
+// import MainApp from './ex03_ServerUrl/MainApp';
+import MainApp from './ex04_X_Data_Grid/MainApp';
 
 function App() {
   return (
