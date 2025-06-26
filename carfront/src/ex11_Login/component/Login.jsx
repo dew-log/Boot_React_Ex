9a0a7491505240ce10b81ed6a863f4d5b7c29a3c
dew <foaming21@gmail.com>
2025-06-26 13:13:10 +0900

    Login 구현

--- carfront/src/ex11_Login/component/Login.jsx
+++ carfront/src/ex11_Login/component/Login.jsx
@@ -3,13 +3,36 @@ import { SERVER_URL } from "./constants";
 import { Button, TextField, Stack } from "@mui/material";
 
 const Login = ()=>{
+    const [user, setUser] = useState({
+        username: "",
+        password: ""
+    });
+    const [isAuthenticated, setAuth] = useState(false);
 
-    return (
+    const handleChange = (e)=>setUser({...user, [e.target.name] : e.target.value});
+
+    const login = ()=>{
+        fetch(SERVER_URL + "login", {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(user)
+        })
+        .then((resp)=>{
+            const jwtToken = resp.headers.get("Authorization");
+            if(jwtToken !== null){
+                sessionStorage.setItem("jwt", jwtToken);
+                setAuth(true);
+            }
+        })
+        .catch((e)=>console.log(e));
+    }
+
+    return(
         <div>
-            <Stack spacing={2} allgnItems="center" mt={2}>
-                <TextField name="username" label="username" />
-                <TextField name="password" label="password" />
-                <Button variant="outlined" color="primary">Login</Button>
+            <Stack spacing={2} alignItems="center" mt={2}>
+                <TextField name="username" label="username" onChange={handleChange} />
+                <TextField name="password" label="password" type="password" onChange={handleChange} />
+                <Button variant="outlined" color="primary" onClick={login}>Login</Button>
             </Stack>
         </div>
     )
